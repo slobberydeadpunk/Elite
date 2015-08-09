9ef3956c1c05bb1f714eda5d2ab5f1b84d15af0d
nan <slobberydeadpunk@gmail.com>
2015-08-09 22:58:56 +0800

    Stack implementation

---
+++ TestMain.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by zhangnan on 14-2-11.
+//  Copyright (c) 2014年 zhangnan. All rights reserved.
+//
+
+#include <iostream>
+#include "Stack.hpp"
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    //test_array_stack();
+    test_chain_stack();
+    
+    return 0;
+}
+
